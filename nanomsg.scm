0da450fbd2f0d907b165b57ea5f4a2e574e831cb
yagi <hiro.yagi0245@gmail.com>
2017-01-02 01:42:02 +0900

    add setsockopt

--- nanomsg.scm
+++ nanomsg.scm
@@ -1,4 +1,4 @@
-(use lolevel foreigners srfi-18)
+(use lolevel foreigners srfi-18 srfi-13 irregex)
 
 #>
 #include <nanomsg/nn.h>
@@ -11,7 +11,6 @@
 <#
 
 ;; TODO: socket options NN_SUB_SUBSCRIBE NN_SUB_UNSUBSCRIBE
-
 (define-record-type nn-socket (%nn-socket-box int)
   nn-socket?
   (int %nn-socket-unbox))
@@ -20,7 +19,7 @@
   %nn-socket-unbox
   %nn-socket-box)
 
-;; nanomsg protocol enum
+;; nanomsg protocol&transport enum
 (define-foreign-enum-type (nn-protocol int)
   (nn-protocol->int int->nn-protocol)
 
@@ -38,6 +37,35 @@
   (raw AF_SP_RAW))
 
 ;; ==================== socket flags
+;; nn-pub
+;; return name and value
+(define (nn-symbol index)
+  (let-location ([value int])
+    (let ([str ((foreign-lambda* c-string ([int i]
+					   [(c-pointer int) value])
+				 "return ("
+				 "nn_symbol(i, value)"
+				 ");")
+		index (location value))])
+      (if str
+	  (cons str value)
+	  #f))))
+
+(define nn-symbol-table
+  (let loop ([acc '()]
+	     [i 0])
+    (let ([pair (nn-symbol i)])
+      (if pair (loop (cons pair acc)
+		     (add1 i))
+	  acc))))
+
+(define (nn-rename str)
+  (string->symbol (string-downcase (irregex-replace/all "_" str "-"))))
+
+(eval (cons 'begin (map (lambda (pair)
+			  `(define ,(nn-rename (car pair))
+			     ,(cdr pair)))
+			nn-symbol-table)))
 
 (define nn/dontwait (foreign-value "NN_DONTWAIT" int))
 
@@ -54,35 +82,56 @@
           (error (nn-strerror) val))
       val))
 
-
 ;; get the pollable fd for socket.
 (define (nn-recv-fd socket)
   (let-location ((fd int -1)
                  (fd_size int (foreign-value "sizeof(int)" int)))
-                (nn-assert
-                 ((foreign-lambda* int ( (nn-socket socket)
-                                    ((c-pointer int) fd)
-                                    ((c-pointer size_t) fds))
-                              "return(nn_getsockopt(socket, NN_SOL_SOCKET, NN_RCVFD, fd, fds));")
-                  socket (location fd) (location fd_size)))
-                (if (not (= (foreign-value "sizeof(int)" int) fd_size))
-                    (error "invalid nn_getsockopt destination storage size" fd_size))
-                fd))
+    (nn-assert
+     ((foreign-lambda* int ( (nn-socket socket)
+			     ((c-pointer int) fd)
+			     ((c-pointer size_t) fds))
+		       "return(nn_getsockopt(socket, NN_SOL_SOCKET, NN_RCVFD, fd, fds));")
+      socket (location fd) (location fd_size)))
+    (if (not (= (foreign-value "sizeof(int)" int) fd_size))
+	(error "invalid nn_getsockopt destination storage size" fd_size))
+    fd))
 
 (define (nn-subscribe socket prefix)
   (nn-assert
    ((foreign-lambda* int ( (nn-socket socket)
-                      (nonnull-blob prefix)
-                      (int len))
-                "return("
-                "nn_setsockopt(socket, NN_SUB, NN_SUB_SUBSCRIBE, prefix, len)"
-                ");")
+			   (nonnull-blob prefix)
+			   (int len))
+		     "return("
+		     "nn_setsockopt(socket, NN_SUB, NN_SUB_SUBSCRIBE, prefix, len)"
+		     ");")
     socket prefix (string-length prefix))))
+
+(define (nn-setsockopt socket level option optval)
+  (nn-assert
+   (cond [(number? optval)
+	  ((foreign-lambda* int ([nn-socket socket]
+				 [int level]
+				 [int option]
+				 [int optval])
+			    "return("
+			    "nn_setsockopt(socket, level, option, &optval, sizeof(optval))"
+			    ");")
+	   socket level option optval)]
+	 [(string? optval)
+	  ((foreign-lambda* int ([nn-socket socket]
+				 [int level]
+				 [int option]
+				 [c-string optval]
+				 [int len])
+			    "return("
+			    "nn_setsockopt(socket, level, option, optval, len)"
+			    ");")
+	   socket level option optval (string-length optval))]
+	 [else
+	  -1])))
 
 (define (nn-close socket)
   (nn-assert ( (foreign-lambda int "nn_close" nn-socket) socket)))
-
-
 
 ;; int nn_socket (int domain, int protocol)
 ;; OBS: args reversed
@@ -106,11 +155,11 @@
 
 (define (nn-send socket data #!optional (flags 0))
   (nn-assert ( (foreign-lambda int "nn_send" nn-socket blob int int)
-              socket data (number-of-bytes data) flags)))
+	       socket data (number-of-bytes data) flags)))
 
 (define (nn-recv! socket data size flags)
   (nn-assert ( (foreign-lambda int "nn_recv" nn-socket (c-pointer void) int int)
-              socket data (or size (number-of-bytes data)) flags)))
+	       socket data (or size (number-of-bytes data)) flags)))
 
 ;; plain nn-recv, will read-block other srfi-18 threads unless
 ;; nn/dontwait flag is specified. returns the next message as a
@@ -119,12 +168,12 @@
   ;; make a pointer which nanomsg will point to its newly allocated
   ;; message
   (let-location
-   ((dst (c-pointer void) #f))
-   (and-let* ((size (nn-recv! socket (location dst) (foreign-value "NN_MSG" int) flags))
-              (blb (make-string size)))
-     (move-memory! dst blb size)
-     (nn-freemsg! dst)
-     blb)))
+      ((dst (c-pointer void) #f))
+    (and-let* ((size (nn-recv! socket (location dst) (foreign-value "NN_MSG" int) flags))
+	       (blb (make-string size)))
+      (move-memory! dst blb size)
+      (nn-freemsg! dst)
+      blb)))
 
 ;; wait for message on socket, return it as string. does not block
 ;; other srfi-18 threads.
